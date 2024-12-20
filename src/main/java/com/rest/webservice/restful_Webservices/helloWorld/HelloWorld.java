b740f1d6b83a2a7e5fce8a4448b8fdcdd70957e8
sudhanshu-Q <sudhanshu.pat.kumar@gmail.com>
2024-12-20 19:25:35 +0530

    different lang

--- src/main/java/com/rest/webservice/restful_Webservices/helloWorld/HelloWorld.java
+++ src/main/java/com/rest/webservice/restful_Webservices/helloWorld/HelloWorld.java
@@ -1,5 +1,9 @@
 package com.rest.webservice.restful_Webservices.helloWorld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+	//to change message in different language
+	private MessageSource messageSource;
+	
+	public HelloWorld(MessageSource messageSource) {
+		this.messageSource=messageSource;
+	}
 	
 	@RequestMapping(method = RequestMethod.GET,path = "/home")
 	public Model helloworld(Model model) {;
@@ -22,6 +32,11 @@ public class HelloWorld {
 	public HelloWorldBean helloworldBean() {
 		return new HelloWorldBean("Hello World Bean");
 	}
-
+	//I18N different language
+	@GetMapping(path = "/helloWorldLang")
+	public String helloworldInternationalization() {
+		Locale locale=LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+	}
 
 }
