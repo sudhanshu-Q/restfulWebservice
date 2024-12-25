9ae80dcdc9a398bae060f7af3cec260c66a2996a
sudhanshu-Q <sudhanshu.pat.kumar@gmail.com>
2024-12-25 14:40:28 +0530

    new row

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,5 @@ insert into USER_DETAILS(BIRTH_DATE,ID,NAME)
 values(current_date(),1105,'DEHG');
 insert into USER_DETAILS(BIRTH_DATE,ID,NAME)
 values(current_date(),1106,'MISS CATR');
+insert into USER_DETAILS(BIRTH_DATE,ID,NAME)
+values(current_date(),1107,'MISS kkATR');
